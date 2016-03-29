fe7eeaf377500c628ff294c4e6ea7dc5bd32d631
Katherine Hegarty <khegarty@smu.edu>
2016-03-29 15:53:30 -0500

    created dummy array of games

--- DedmanPick/www/js/services.js
+++ DedmanPick/www/js/services.js
@@ -6,26 +6,36 @@ angular.module('starter.services', [])
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Basketball'
+    name: 'Basketball',
+    place: 'Dedman',
+    time: '12:30 PM'
+  
   }, {
     id: 1,
-    name: 'Soccer'
+    name: 'Soccer',
+    place:'Intramural field',
+    time: '6:00 PM'
   }, {
     id: 2,
     name: 'Tennis',
+    place: 'Tennis Courts',
+    time: '9:00 AM'
     
   }, {
     id: 3,
-    name: 'Ping Pong'
+    name: 'Ping Pong',
+    place: 'Dedman',
+    time: '2:45 PM'
   }, {
     id: 4,
-    name: 'Pool'  
+    name: 'Pool',
+    place: 'Dedman',
+    time: '3:30 PM'
   },{
     id: 5,
-    name: 'Sand Volleyball'
-  },{
-    id: 1,
-    name: 'Other'
+    name: 'Sand Volleyball',
+    place: 'Sand Courts',
+    time: '8:45 PM'
   }];
 
   return {
