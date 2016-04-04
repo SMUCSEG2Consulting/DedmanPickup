6618ea5978545f204fc0ff7342a50cc73263db70
iqbalyarkhan <iykhan@smu.edu>
2016-04-04 17:53:46 -0500

    Update index.php

--- html/public/index.php
+++ html/public/index.php
@@ -1,8 +1,8 @@
 <?php
 //Headers to remove CORS errors:
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Origin: GET, POST, OPTIONS');
-header('Access-Control-Allow-Origin: Content');
+header('Access-Control-Allow-Origin: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Origin: Content-Type');
 header('Access-Control-Allow-Origin: X-Requested-With');
 if (PHP_SAPI == 'cli-server') {
     // To help the built-in PHP dev server, check if the request was actually for
